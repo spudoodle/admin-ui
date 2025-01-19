c3abb0501c87c331944a674c7bf40495a2b85523
spudoodle <achaldev3@gmail.com>
2025-01-19 12:52:32 +0530

    refactor: logout function

--- src/app/(root)/settings/page.tsx
+++ src/app/(root)/settings/page.tsx
@@ -2,17 +2,15 @@
 
 import { useAuthContext } from "@/context/useAuthContext";
 import { LogoutIcon } from "@/utils/icons";
-import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function Settings() {
   const { setAuthenticated } = useAuthContext();
-  const router = useRouter();
 
   const handleLogout = () => {
+    document.cookie = `jiitak-auth-cookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Lax;`;
     setAuthenticated(false);
-    document.cookie = `jiitak-auth-cookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-    router.push("/login");
+    window.location.href = "/login";
   };
 
   return (
